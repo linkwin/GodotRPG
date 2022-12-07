68d52ff9ce657c34725513263f11963c9b425e69
linkwin <alanscholar5@gmail.com>
2022-12-07 12:10:36 -0500

    Add FuncLib global script for ease of access to common general purpose functions, used to fixed issue with general scene switching. Started testing GUI

--- Scripts/FuncLib.gd
+++ Scripts/FuncLib.gd
@@ -2,9 +2,10 @@ extends Node
 
 var current_scene_name = "LocalEntities"
 
+onready var world = get_tree().get_root().get_node("World")
+
 func switch_scene(new_scene_path):
 	var new_scene = load(new_scene_path).instance()
-	var world = get_tree().get_root().get_node("World")
 	var player = world.get_node("TranientEntities/Player")
 	var current_scene = world.get_node(current_scene_name)
 	
@@ -12,3 +13,13 @@ func switch_scene(new_scene_path):
 	world.add_child(new_scene)
 	player.position = world.get_node(new_scene.name + "/PlayerStart").position
 	current_scene_name = new_scene.name
+
+func show_one_child(parent, child):
+	var parent_node = parent
+	for node in parent_node.get_children():
+		node.hide()
+	child.show()
+	
+func hide_all_children(parent):
+	for node in parent.get_children():
+		node.hide()
