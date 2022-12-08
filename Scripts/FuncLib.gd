7f0e2b2674e94f0e9cfbc4a0e0f6bef25f629551
linkwin <alanscholar5@gmail.com>
2022-12-08 13:08:27 -0500

    Updated FuncLib script with scene caching

--- Scripts/FuncLib.gd
+++ Scripts/FuncLib.gd
@@ -1,18 +1,71 @@
 extends Node
+var save_file = "res://db/savedscene.tscn"
+
 
 var current_scene_name = "LocalEntities"
+
+var cached_scenes = []
 
 onready var world = get_tree().get_root().get_node("World")
 
 func switch_scene(new_scene_path):
-	var new_scene = load(new_scene_path).instance()
+	var new_scene
+	for scene in cached_scenes:
+		if new_scene_path.find(scene.name) >= 0:
+			new_scene = scene
+	if new_scene == null:
+		new_scene = load(new_scene_path).instance()
 	var player = world.get_node("TranientEntities/Player")
 	var current_scene = world.get_node(current_scene_name)
-	
+	cached_scenes.append(current_scene)
 	world.remove_child(current_scene)
 	world.add_child(new_scene)
 	player.position = world.get_node(new_scene.name + "/PlayerStart").position
 	current_scene_name = new_scene.name
+	
+func save_node(node):
+	print("saving " + str(node.name) + " node...")
+	set_children_owned(node)
+	var scene_pack = PackedScene.new()
+	scene_pack.pack(node)
+	ResourceSaver.save(save_file, scene_pack)
+	
+func load_saved_scene():
+	var scene_instance = load(save_file).instance()
+	get_tree().get_root().remove_child(world)
+	get_tree().get_root().add_child(scene_instance)
+	#world.remove_child(world.get_node(scene_instance.name))
+	#world.get_node(scene_instance.name).queue_free()
+	#world.add_child(load(save_file).instance())
+	
+func load_world_nodes(world_nodes_save):
+	if (world_nodes_save != null):
+		print("loading saved world...")
+		var root = get_tree().get_root()
+		for node in world.get_children():
+			world.remove_child(node)
+		for node in world_nodes_save:
+			print(node)
+			world.add_child(node.instance())
+		#root.remove_child(world)
+		#world.queue_free()
+		#root.add_child(world_save_instance)
+		world = get_tree().get_root().get_node("World")
+	
+func load_world(world_save):
+	if (world_save != null):
+		print("loading saved world...")
+		var world_save_instance = world_save.instance()
+		var root = get_tree().get_root()
+		for node in world.get_children():
+			world.remove_child(node)
+		for node in world_save_instance.get_children():
+			print(node)
+			world.add_child(node)
+		#root.remove_child(world)
+		#world.queue_free()
+		#root.add_child(world_save_instance)
+		world = get_tree().get_root().get_node("World")
 
 func show_one_child(parent, child):
 	var parent_node = parent
@@ -23,3 +76,7 @@ func show_one_child(parent, child):
 func hide_all_children(parent):
 	for node in parent.get_children():
 		node.hide()
+		
+func set_children_owned(parent):
+	for node in parent.get_children():
+		node.set_owner(parent)
