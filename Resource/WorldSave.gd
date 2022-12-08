ee8466dee3530e5ac804795a652da81f3245cf81
linkwin <alanscholar5@gmail.com>
2022-12-08 01:29:26 -0500

    Almost done with saving and loading world state

--- Resource/WorldSave.gd
+++ Resource/WorldSave.gd
@@ -1,24 +1,21 @@
 extends Resource
 class_name WorldSave
 
-export(PackedScene) var world_save
 export(Array) var world_nodes_save
+export var save_path = "res://Resource/WorldSaveState.tres"
 
-var save_file = "res://db/savedscene.tscn"
+var world_entities = []
 
 func save_world(world_scene):
 	print("Attempting to save world")
 	world_nodes_save = []
-	var i = 0
+
 	for node in world_scene.get_children():
-		world_nodes_save.append(PackedScene.new())
-		world_nodes_save[i].pack(node)
-		i += 1
-	world_save = PackedScene.new()
-	world_save.pack(world_scene)
+		var scene_pack = PackedScene.new()
+		scene_pack.pack(node)
+		var path = "res://db/" + str(node.name) + ".tscn"
+		world_nodes_save.append(path)
+		ResourceSaver.save(path, scene_pack)
 	
-	ResourceSaver.save(save_file, world_save)
-	ResourceSaver.save("res://Resource/WorldSaveState.tres", self)
+	ResourceSaver.save(save_path, self)
 	
-func get_world_save() -> PackedScene:
-	return world_save
